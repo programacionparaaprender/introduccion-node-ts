08b6318089d6ccede0e0f6ecd880b0456260ceb9
programacionparaaprender <yancel209@gmail.com>
2023-03-18 11:25:51 -0500

    primer commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # introduccion-node-ts
 Ejemplo de curso de nodejs
+
+
+
+```npm set init-author-email "example-user@example.com"```
+```npm set init-author-name "example_user"```
+```npm set init-license "MIT"```
+```npm config list```
+```tsc --init```
---
+++ lib/index.ts
@@ -0,0 +1,23 @@
+export function hola() {
+    console.log('Hola Mundo!!!');
+}
+
+export function holaPersonalizado(nombre: string) {
+    console.log(`Hola ${ nombre }`);
+}
+
+export function holaAlCursoNPM(){
+    console.log('Hola al curso NPM!!!');
+}
+
+export function holaAlCursoNPMPrueba(){
+    console.log('Hola al curso NPM Prueba 1!!!');
+}
+
+export function holaAlCursoNPMPrueba2(){
+    console.log('Hola al curso NPM Prueba 2!!!');
+}
+
+export function holaAlCursoNPMPrueba3(){
+    console.log('Hola al curso NPM Prueba 3!!!');
+}
